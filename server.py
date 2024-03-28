c498bd8adb719d5d085ee9dd03a2ac520be31959
Fernando Figaro <70062249+Fernando-olv@users.noreply.github.com>
2024-03-28 07:09:12 +0000

    Removed comments from code

--- server.py
+++ server.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 @app.route('/city/<city_name>', methods=['GET'])
 def city(city_name):
     return city_name
-##To do, get the city_name variable from the Get path ex: city/Curitiba instead of using the request.args
-##Now you can use the city's name refering to {city_name}
 
 @app.route('/coordinates', methods=['GET'])
 def coordinates():
